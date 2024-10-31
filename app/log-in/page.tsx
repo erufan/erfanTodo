c085993850d2ab4cc4674054be8ff06e243bb92b
erfan taheri <erfanta197@gmail.com>
2024-10-31 20:43:31 +0330

    feat: add UI of login form

---
+++ app/log-in/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+const LoginForm = () => {
+  const { Item } = Form;
+  return (
+    <Form
+      name="login_form"
+      className="login-form"
+      initialValues={{ remember: true }}
+      style={{ maxWidth: "300px", margin: "auto", paddingTop: "50px" }}
+    >
+      <Item
+        name="username"
+        rules={[{ required: true, message: "Please input your Username!" }]}
+      >
+        <Input prefix={<UserOutlined />} placeholder="Username" />
+      </Item>
+      <Item
+        name="password"
+        rules={[{ required: true, message: "Please input your Password!" }]}
+      >
+        <Input
+          prefix={<LockOutlined />}
+          type="password"
+          placeholder="Password"
+        />
+      </Item>
+      <Item style={{ display: "flex", justifyContent: "center" }}>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+      </Item>
+    </Form>
+  );
+};
+
+export default LoginForm;
