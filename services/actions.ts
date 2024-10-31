879bbdd1f0bd9a48a8ae2714d4aacac5d23c9977
erfan taheri <erfanta197@gmail.com>
2024-10-31 21:21:48 +0330

    feat: add fetch request to server action for login

--- services/actions.ts
+++ services/actions.ts
@@ -1,5 +1,24 @@
 "use server";
 
-export async function logIn(FormData: User) {
-  console.log(FormData);
+export async function logIn(user: User) {
+  try {
+    const response = await fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Login error:", error);
+    return { success: false, message: "An error occurred. Please try again." };
+  }
 }
